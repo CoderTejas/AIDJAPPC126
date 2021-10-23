0fdb32676fc684848d3044f73434626fa09aa543
CoderTejas <75924121+CoderTejas@users.noreply.github.com>
2021-10-23 00:16:00 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
@@ -12,6 +16,28 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('Pose Net Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristX = results[0].pose.leftWrist.y;
+        console.log("x coordinate of left wrist" + leftWristX + "y coordinate of left wrist" + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristX = results[0].pose.rightWrist.y;
+        console.log("x coordinate of right wrist" + rightWristX + "y coordinate of right wrist" + rightWristY);
+    }
 }
 
 function draw()
@@ -22,4 +48,6 @@ function draw()
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
