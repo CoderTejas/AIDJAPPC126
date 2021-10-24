09b7894c456767e983fa00ebd164980667b14c47
CoderTejas <75924121+CoderTejas@users.noreply.github.com>
2021-10-24 00:31:55 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 song = "";
+scoreLeftWrist = 0;
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
@@ -31,6 +32,8 @@ function gotPoses(results)
     if(results.length>0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("Score left Wrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("x coordinate of left wrist" + leftWristX + "y coordinate of left wrist" + leftWristY);
@@ -45,12 +48,15 @@ function draw()
     image(video, 0, 0, 600, 500);
     fill("#f26100");
     stroke("#f26100");
+    if(scoreLeftWrist>0.2)
+    {
     circle(leftWristX, leftWristY, 20);
     in_number_leftWristY = Number(leftWristY);
     remove_decimals = floor(in_number_leftWristY);
     volume = remove_decimals/500;
     document.getElementById("volume").innerHTML = "Volume = " + volume;
     song.setVolume();
+    }
 }
 
 function play()
