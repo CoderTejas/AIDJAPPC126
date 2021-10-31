58adffb6b5a43530ed33af7b16f33efb5ace0132
CoderTejas <75924121+CoderTejas@users.noreply.github.com>
2021-10-30 23:09:41 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -48,6 +48,34 @@ function draw()
     image(video, 0, 0, 600, 500);
     fill("#f26100");
     stroke("#f26100");
+    circle(rightWristX, rightWristY, 20);
+
+    if(rightWristY>0 && rightWristY<=100)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if(rightWristY>100 && right<=200)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1x";
+        song.rate(1);
+    }
+    else if(rightWristY>200 && rightWristY<=300)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY>300 && rightWristY<=400)
+    {
+        document.getElementById("speed").innerHTMl = "Speed = 2x";
+        song.rate(2);
+    }
+    else if(rightWristY>400 && rightWristY<=500)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2.5x";
+        song.rate(2.5);
+    }
+
     if(scoreLeftWrist>0.2)
     {
     circle(leftWristX, leftWristY, 20);
